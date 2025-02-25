658331a76d52a73f7e94c096e7dc893a90b95e2a
kennethazin <kenneth.ras3@mail.dcu.ie>
2025-02-25 20:59:14 +0000

    Refactor: adjust canvas dimensions and remove unused imports; add checklist component

---
+++ src/app/checklist/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Checklist = () => {
+  return <div className="text-4xl">This is a checklist</div>;
+};
+
+export default Checklist;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Suspense, useEffect, useRef } from "react";
+import { Suspense, useRef } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Model } from "@/components/moon";
@@ -34,34 +34,36 @@ export default function App() {
 
   return (
     <>
-      <Canvas
-        className="bg-black"
-        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 3] }}
-      >
-        <Stars
-          radius={100}
-          depth={200}
-          count={10000}
-          factor={10}
-          saturation={1}
-          fade
-          speed={1}
-        />
-        <OrbitControls
-          autoRotate
-          rotateSpeed={0.5}
-          zoomSpeed={1}
-          autoRotateSpeed={0.1}
-        />
-        <Suspense fallback={null}>
-          <Model ref={moonRef} />
-          <Earth position={[100, 20, 300]} />
-          <Telemetry moonRef={moonRef} />
-          <CameraCollision moonRef={moonRef} />
-          <Sun position={[-80, 400, 800]} />
-        </Suspense>
-        <mesh />
-      </Canvas>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Canvas
+          className="bg-black "
+          camera={{ fov: 50, near: 0.1, far: 1000, position: [0, 0, 3] }}
+        >
+          <Stars
+            radius={100}
+            depth={200}
+            count={10000}
+            factor={10}
+            saturation={1}
+            fade
+            speed={1}
+          />
+          <OrbitControls
+            autoRotate
+            rotateSpeed={0.5}
+            zoomSpeed={1}
+            autoRotateSpeed={0.1}
+          />
+          <Suspense fallback={null}>
+            <Model ref={moonRef} />
+            <Earth position={[100, 20, 300]} />
+            <Telemetry moonRef={moonRef} />
+            <CameraCollision moonRef={moonRef} />
+            <Sun position={[-80, 400, 800]} />
+          </Suspense>
+          <mesh />
+        </Canvas>
+      </div>
     </>
   );
 }
