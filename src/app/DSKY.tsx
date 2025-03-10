96d1fd392ae4cac37bea4e29150fe63b4a3ba017
kennethazin <kenneth.ras3@mail.dcu.ie>
2025-03-10 23:40:06 +0000

    Increase DSKY component height and update font sizes for improved readability

--- src/app/DSKY.tsx
+++ src/app/DSKY.tsx
@@ -5,7 +5,7 @@ import DSKYNumericPanel from "./components/DSKYNumericPanel";
 
 const DSKY = () => {
   return (
-    <div className="bg-gray-200 w-[350px] h-[450px] p-1 flex flex-col text-center font-mono rounded-sm ">
+    <div className="bg-gray-200 w-[350px] h-[470px] p-1 flex flex-col text-center font-mono rounded-sm ">
       <div className="flex flex-row justify-evenly m-2 h-3/5 mx-4 gap-2 ">
         <div className="w-full h-full  border-[0.1rem] border-neutral-600  rounded-lg   ">
           <div className="w-full h-full mx-0 grid grid-cols-2 grid-rows-7 gap-1 rounded-md border-[0.3rem]  border-neutral-400 p-0.5 shadow-[inset_4px_-2px_5px_rgba(4,4,0,0.5)]  ">
@@ -25,7 +25,7 @@ const DSKY = () => {
           <div className="relative w-full h-full mx-0  rounded-md border-[0.3rem]    border-neutral-400 p-0.5 shadow-[inset_4px_-2px_5px_rgba(4,4,0,0.5)]  bg-green-950/30   ">
             <div className="grid mb-1">
               <div className="text-xs uppercase  flex flex-row justify-between items-center ">
-                <div className="  bg-green-400 w-2/5 h-full flex flex-col items-center justify-center rounded-sm ">
+                <div className="  bg-green-400 w-2/5 h-full flex flex-col items-center justify-center rounded-sm  ">
                   <div>comp</div>
                   <div>acty</div>
                 </div>
@@ -33,7 +33,7 @@ const DSKY = () => {
                   <div className="bg-green-400 w-full rounded-sm text-xs">
                     prog
                   </div>
-                  <div className="text-4xl ds-digital text-green-400">00</div>
+                  <div className="text-5xl ds-digital text-green-400">00</div>
                 </div>
               </div>
             </div>
@@ -46,16 +46,16 @@ const DSKY = () => {
               </div>
             </div>
             <div className="flex flex-row justify-between items-center">
-              <div className="text-4xl ds-digital text-green-400">00</div>
-              <div className="text-4xl ds-digital text-green-400">00</div>
+              <div className="text-5xl ds-digital text-green-400">00</div>
+              <div className="text-5xl ds-digital text-green-400">00</div>
             </div>
             <div className="flex flex-col space-y-[-3px]">
               <div className="bg-green-300 h-[1px]" />
-              <div className="text-4xl ds-digital text-green-400">+00000</div>
+              <div className="text-5xl ds-digital text-green-400">+00000</div>
               <div className="bg-green-300 h-[1px]" />
-              <div className="text-4xl ds-digital text-green-400">+00000</div>
+              <div className="text-5xl ds-digital text-green-400">+00000</div>
               <div className="bg-green-300 h-[1px]" />
-              <div className="text-4xl ds-digital text-green-400">+00000</div>
+              <div className="text-5xl ds-digital text-green-400">+00000</div>
             </div>
           </div>
         </div>
