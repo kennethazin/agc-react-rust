5d0552ff3c001ac4ee8ab7a8369359f85338d938
kennethazin <kenneth.ras3@mail.dcu.ie>
2025-03-12 12:32:24 +0000

    beginning agccontent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import GuidancePanel from "./GuidancePanel";
 export default function Home() {
   return (
     <>
+      <div className="absolute text-white h-screen w-screen z-0"></div>
       <div className=" w-full flex  flex-row items-end  min-h-screen bg-black">
         <div className=" w-full flex items-center justify-between gap-0 p-2 ">
           <div className="w-[350px] h-[470px] shadow-sm z-20">
             <DSKY />
           </div>
-          <div className="h-full  shadow-sm bg-gray-300 rounded-sm py-1 z-auto">
+          <div className="h-full  shadow-sm bg-gray-300  rounded-sm py-1 z-50">
             <GuidancePanel />
           </div>
         </div>
